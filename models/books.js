29dd3101d28380d3057b3fd59c55a52ee10b6583
Carlos <carlosbenitez86753@yahoo.com>
2020-12-03 16:47:06 -0500

    added a schema

---
+++ models/books.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const bookSchema = new Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    authors:{
+        type: Array,
+        required: true
+    },
+    description:{
+        type:String,
+        required: true
+    },
+    image:{
+        type: String
+    },
+    link:{
+        type: String
+    }
+})
